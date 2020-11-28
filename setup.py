9d078d055014333dab98e8a196c45a205356a48c
Kuang Wei <kuang.wei@stong.io>
2020-11-27 19:54:37 -0600

    Add additional space in entry point

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='automoneylover',
                         ],
       entry_points={
           'console_scripts': [
-              'automoneylover:log= automoneylover.main:main',
+              'automoneylover:log = automoneylover.main:main',
           ]
       }
  )
