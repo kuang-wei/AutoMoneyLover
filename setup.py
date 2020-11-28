9cf77be2565a35ef9f0a448de6738491952acc12
Kuang Wei <kuang.wei@stong.io>
2020-11-27 19:51:55 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='automoneylover',
+      version='0.0.1',
+      description='Automate Money Lover transaction logging',
+      author='Kuang Wei',
+      author_email='kuangwei0824@gmail.com',
+      packages=['automoneylover'],
+      install_requires=['mintapi',
+                        'keyring',
+                        ],
+      entry_points={
+          'console_scripts': [
+              'automoneylover:log= automoneylover.main:main',
+          ]
+      }
+ )
